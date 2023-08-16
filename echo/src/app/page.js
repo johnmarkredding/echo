2a37b232fab33272b9f46be6a88418fd7467de63
John Mark <johomurk@gmail.com>
2023-08-16 19:49:25 +0000

    Found and solved issue with form submit not changing state

--- echo/src/app/page.js
+++ echo/src/app/page.js
@@ -1,95 +1,30 @@
-import Image from 'next/image'
+'use strict'
+'use client'
 import styles from './page.module.css'
+import { useState, useEffect } from 'react'
 
-export default function Home() {
+export default () => {
+  useEffect(() => {
+    // get messages in my area - dep: my location;
+  },[]);
+  const [messages, setMessages] = useState([{id: 1, text:"hey"}]);
+  const [echoInput, setEchoInput] = useState("");
+  const sendNewEcho = (e) => {
+    e.preventDefault();
+    const newMessages = [{id: messages.length + 1, text: echoInput}, ...messages];
+    setEchoInput("");
+    setMessages(newMessages);
+  };
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1>Echo</h1>
+      <ol>
+        { messages.map(m => <li key={m.id}>{m.text}</li>) }
+      </ol>
+      <form onSubmit={sendNewEcho}>
+        <label htmlFor="echo-input">your echo</label>
+        <input required id="echo-input" onChange={e => setEchoInput(e.target.value)} placeholder={'There\u2019s a snake…'} type='text' value={echoInput}/>
+      </form>
     </main>
   )
 }
