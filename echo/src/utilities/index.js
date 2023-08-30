d5f4a0063d202761c9dcc03d9ba1ee8248b604ff
John Mark Redding <johomurk@gmail.com>
2023-08-30 14:07:19 -0500

    Export postEcho.js from utilities folder

--- echo/src/utilities/index.js
+++ echo/src/utilities/index.js
@@ -1,3 +1,4 @@
 export {default as createGeolocationObservable} from "./geolocationObservable";
 export {default as createPermissionsObservable} from "./permissionsObservable";
 export {default as getEchoes} from "./getEchoes";
+export {default as postEcho} from "./postEcho";
---
+++ echo/src/utilities/postEcho.js
@@ -0,0 +1,10 @@
+export default (echo) => {
+    console.log(echo);
+    // const response = await fetch(process.env.GRAPHQL_API_URL, {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json", accept: "application/json" },
+    //   // body: JSON.stringify({ query: ---------- }) // gotta write the query once the backend exists.
+    // });
+    // return await response.json();
+    return {...echo, id: Math.random()};
+};
