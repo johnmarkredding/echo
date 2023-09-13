18ed6b32d3a1cc67320b5635b482734786b289d6
John Mark Redding <johomurk@gmail.com>
2023-09-13 17:34:29 -0500

    Remove console.log of newEcho data

--- echo/src/helpers/handleNewEcho.js
+++ echo/src/helpers/handleNewEcho.js
@@ -2,7 +2,6 @@ import postEcho from "./postEcho";
 export default (e, text, {latitude, longitude}, setText) => {
   e.preventDefault();
   postEcho({text, coords: {latitude, longitude}})
-    .then((echo) => {console.log(echo.data)})
     .catch((err) => {console.error("Not possible", err)})
     .finally(() => {setText("")});
 }
