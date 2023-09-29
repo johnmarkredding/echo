f3cb2e8a0760dfa4e77b0416320ae8c3fa48a5b7
John Mark Redding <johomurk@gmail.com>
2023-09-29 15:54:39 -0700

    Remove unnecessary setup code, customize markers

--- echo/src/app/components/GoogleMap.js
+++ echo/src/app/components/GoogleMap.js
@@ -1,7 +1,8 @@
 'use strict'
 'use client'
-import React, { memo, useState, useCallback } from 'react'
+import React, { memo, useCallback } from 'react';
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
 const NEXT_PUBLIC_GMAPS_KEY = process.env.NEXT_PUBLIC_GMAPS_KEY;
 const NEXT_PUBLIC_GMAPS_MAP_ID = process.env.NEXT_PUBLIC_GMAPS_MAP_ID;
 
@@ -13,40 +14,23 @@ const defaultMapOptions = {
   mapId: NEXT_PUBLIC_GMAPS_MAP_ID,
   disableDefaultUI: true,
   minZoom: 16,
-  maxZoom: 18,
   gestureHandling: "none"
 };
 
-export default memo(
-  ({
-    center = null,
-    mapContainerStyle,
-    mapOptions
-  }) => {
+export default memo(({center = null, mapContainerStyle, mapOptions, markers = [] }) => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: NEXT_PUBLIC_GMAPS_KEY
-  })
+  });  
 
-  const [map, setMap] = useState(null)
-
-  const onLoad = useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+  const onLoad = useCallback(function callback(map) {}, [])
+  const onUnmount = useCallback(function callback(map) {}, [])
 
   return isLoaded && center ? (
       <GoogleMap
         mapContainerStyle={{...defaultMapContainerStyle, ...mapContainerStyle}}
         center={center}
-        zoom={15}
+        zoom={17}
         onLoad={onLoad}
         onUnmount={onUnmount}
         options={{...defaultMapOptions, ...mapOptions}}
@@ -54,9 +38,12 @@ export default memo(
         { /* Child components, such as markers, info windows, etc. */ }
         <>
           <Marker
+            title={"Your position"}
             key={"User Location"}
             position={center}
+            icon={"./marker.svg"}
           />
+          { markers.map(((m, i) => <Marker key={i} position={m.position} icon={"./marker.svg"} />))}
         </>
       </GoogleMap>
   ) : <></>
