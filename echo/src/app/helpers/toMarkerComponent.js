1f88e610bcfb19f63956885098201c18d5b7d9de
John Mark Redding <johomurk@gmail.com>
2023-10-02 22:42:12 -0700

    Remove traces of old implementation

--- echo/src/app/helpers/toMarkerComponent.js
+++ echo/src/app/helpers/toMarkerComponent.js
@@ -6,7 +6,6 @@ export default ({coords: {latitude: lat, longitude: lng}, id, ...rest}) => {
       title={"Echo"}
       key={id}
       position={{lat, lng}}
-      icon={"./marker.svg"}
       {...rest}
     />
   );
