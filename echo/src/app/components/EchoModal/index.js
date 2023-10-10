b6acb1130f405b416613a141c66a506ee3e8c342
John Mark Redding <johomurk@gmail.com>
2023-10-09 23:58:31 -0700

    Layout EchoModal for echo details

--- echo/src/app/components/EchoModal/index.js
+++ echo/src/app/components/EchoModal/index.js
@@ -5,10 +5,18 @@ const EchoModal = ({echoes, ...rest}) => {
   return echoes?.length > 0
     ? (
       <article {...rest} className={styles.modal} id="echo-modal" onClick={(e) => e.stopPropagation()}>
-        {echoes.map((echo) => <p key={echo.id}>{echo.text}</p>)}
+        {echoes.map((echo) => <EchoBlock {...echo} key={echo.id}/>)}
       </article>
     )
     : null;
 };
+
+const EchoBlock = ({text, coords: {longitude, latitude}, timestamp}) => (
+  <div>
+    <p>{text}</p>
+    <span>{latitude.toFixed(6) + ', ' + longitude.toFixed(6)}</span>
+    <span>{Math.trunc((Date.now() - Date.parse(timestamp))/60000)}min ago</span>
+  </div>
+);
 
 export default EchoModal;
