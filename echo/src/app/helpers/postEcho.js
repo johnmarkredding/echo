62ca4acc67ac3584c39acbcae0b62c2622167a62
John Mark Redding <johomurk@gmail.com>
2023-10-19 22:29:17 -0700

    Resolve ESLint single quote rule

--- echo/src/app/helpers/postEcho.js
+++ echo/src/app/helpers/postEcho.js
@@ -1,16 +1,16 @@
 export default async (echo) => {
   try {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_SERVER_URL + "/echo", {
-      method: "POST",
+    const response = await fetch(process.env.NEXT_PUBLIC_API_SERVER_URL + '/echo', {
+      method: 'POST',
       headers: { 
-        "Content-Type": "application/json", 
-        accept: "application/json"
+        'Content-Type': 'application/json', 
+        accept: 'application/json'
       },
       body: JSON.stringify({data: echo})
     });
     return await response.json();
   } catch (err) {
-    console.error("Error sending echo", err);
+    console.error('Error sending echo', err);
     return null;
   }
 };
