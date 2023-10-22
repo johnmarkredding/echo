343e2d91b0fd89ffd2c675a2d50c0e22407879f6
John Mark Redding <johomurk@gmail.com>
2023-10-21 17:04:03 -0700

    Remove extra space to match ESLint rules

--- src/app/helpers/handleNewEcho.js
+++ src/app/helpers/handleNewEcho.js
@@ -1,7 +1,7 @@
 import postEcho from './postEcho';
 export default (e, text, {latitude, longitude}, setText) => {
   e.preventDefault();
-  postEcho({text, coords: {latitude, longitude} })
+  postEcho({text, coords: {latitude, longitude}})
     .catch((err) => {console.error('Not possible', err)})
     .finally(() => {setText('')});
 };
