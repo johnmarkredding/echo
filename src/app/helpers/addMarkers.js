ff5dcbf37560ad5fc5496cdf646eb11ed4ac52bb
John Mark Redding <johomurk@gmail.com>
2023-11-29 21:41:46 -0800

    Swap cluster Markers to AdvancedMarkerElements

--- src/app/helpers/addMarkers.js
+++ src/app/helpers/addMarkers.js
@@ -1,7 +1,7 @@
 /*global google*/
 'use strict';
 import {MarkerClusterer, SuperClusterAlgorithm} from '@googlemaps/markerclusterer';
-import {generateMarkerIcon} from '@/app/helpers';
+import {generateMarkerIcon, generateClusterIcon} from '@/app/helpers';
 
 const addMarkers = (map, data, listenerCallback) => {
   const markers = data.map((echo) => {
@@ -27,32 +27,13 @@ const addMarkers = (map, data, listenerCallback) => {
     render: (cluster, stats, map) => {
       // change color if this cluster has more markers than the mean cluster
       const color = cluster.count > Math.max(10, stats.clusters.markers.mean)
-        ? '#444'
+        ? '#0f0'
         : 'rgba(0, 0, 0, 0.80)';
 
-      // create svg url with fill color
-      const svg = window.btoa(`
-      <svg fill="${color}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 240">
-        <circle cx="120" cy="120" opacity=".6" r="70" />
-        <circle cx="120" cy="120" opacity=".3" r="90" />
-        <circle cx="120" cy="120" opacity=".2" r="110" />
-        <circle cx="120" cy="120" opacity=".1" r="130" />
-      </svg>`);
-
-      // create marker using svg icon
-      return new google.maps.Marker({
+      return new google.maps.marker.AdvancedMarkerElement({
         position: cluster.position,
         map,
-        icon: {
-          url: `data:image/svg+xml;base64,${svg}`,
-          scaledSize: new google.maps.Size(35, 35),
-        },
-        label: {
-          text: String(cluster.count),
-          color: 'rgba(255,255,255,0.9)',
-          fontSize: '12px',
-        },
-        // adjust zIndex to be above other markers
+        content: generateClusterIcon(color, cluster.count),
         zIndex: 1000 + cluster.count,
       });
     }
