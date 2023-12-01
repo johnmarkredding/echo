fe35193ed28790ec3b16615fd78a4a64aab48eb2
John Mark Redding <johomurk@gmail.com>
2023-11-30 17:41:15 -0800

    Avoid renderer re-creation on addMarkers() call.

--- src/app/helpers/addMarkers.js
+++ src/app/helpers/addMarkers.js
@@ -3,41 +3,37 @@
 import {MarkerClusterer, SuperClusterAlgorithm} from '@googlemaps/markerclusterer';
 import {generateMarkerIcon, generateClusterIcon} from '@/app/helpers';
 
-const addMarkers = (map, data, listenerCallback) => {
-  const markers = data.map((echo) => {
-    const {
-      coords: {latitude: lat, longitude: lng},
-      id
-    } = echo;
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-      title: 'Echo ' + id,
+const renderer = {
+  render: (cluster, stats, map) => {
+
+    return new google.maps.marker.AdvancedMarkerElement({
+      position: cluster.position,
       map,
-      position: {lat, lng},
-      content: generateMarkerIcon()
+      content: generateClusterIcon(cluster.count),
+      zIndex: 1000 + cluster.count,
     });
-    marker.echo = echo;
-    marker.addListener('click', (e) => {
-      e.stop();
-      listenerCallback([echo]);
-    });
-    return marker;
+  }
+};
+
+const addMarker = (map, echo, listenerCallback) => {
+  const {coords: {latitude: lat, longitude: lng}, id} = echo;
+  
+  const marker = new google.maps.marker.AdvancedMarkerElement({
+    title: 'Echo ' + id,
+    map,
+    position: {lat, lng},
+    content: generateMarkerIcon()
   });
+  marker.echo = echo;
+  marker.addListener('click', (e) => {
+    e.stop();
+    listenerCallback([echo]);
+  });
+  return marker;
+};
 
-  const renderer = {
-    render: (cluster, stats, map) => {
-      // change color if this cluster has more markers than the mean cluster
-      const color = cluster.count > Math.max(10, stats.clusters.markers.mean)
-        ? '#0f0'
-        : 'rgba(0, 0, 0, 0.80)';
-
-      return new google.maps.marker.AdvancedMarkerElement({
-        position: cluster.position,
-        map,
-        content: generateClusterIcon(color, cluster.count),
-        zIndex: 1000 + cluster.count,
-      });
-    }
-  };
+const addMarkers = (map, data, listenerCallback) => {
+  const markers = data.map((echo) => addMarker(map, echo, listenerCallback));
 
   const markerClusterer = new MarkerClusterer({
     algorithm: new SuperClusterAlgorithm({maxZoom: 22, radius: 60}),
