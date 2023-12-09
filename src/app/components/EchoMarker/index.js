9e9dca440713b78597be785d3abba85eb2f9262e
John Mark Redding <johomurk@gmail.com>
2023-12-08 21:29:43 -0800

    Remove memo, allow use with map or clusterer

--- src/app/components/EchoMarker/index.js
+++ src/app/components/EchoMarker/index.js
@@ -1,35 +1,61 @@
 /*global google*/
 'use strict';
 'use client';
-import {memo, useEffect, useContext} from 'react';
+import {useRef, useEffect, useContext} from 'react';
+import {useMap} from '@vis.gl/react-google-maps';
 import {generateMarkerIcon} from '@/app/helpers';
 import {ClustererContext} from '@/app/contexts';
 
-const EchoMarker = memo(({echo, onClick}) => {
+const EchoMarker = ({echo, onClick}) => {
   const clusterer = useContext(ClustererContext);
-
+  const map = useMap();
+  const marker = useRef(null);
+  
+  // Add marker to map, if not inside a clusterer
   useEffect(() => {
-    const newMarker = new google.maps.marker.AdvancedMarkerElement({
-      title: 'Echo ' + echo.id,
-      position: {lat: echo.coords.latitude, lng: echo.coords.longitude},
-      content: generateMarkerIcon()
-    });
-    newMarker.echo = echo;
+    if (clusterer || !marker.current || !map) {return}
+    marker.current.map = map;
+    return () => {marker.current.map = null};
+  }, [map, marker, clusterer]);
 
-    const markerClickListener = newMarker.addListener('click', (e) => {
+  // Add marker to clusterer, if one exists
+  useEffect(() => {
+    if (!clusterer || !marker.current) {return}
+    const currentClusterer = clusterer.current;
+      
+    // Check that ref has a clusterer instance
+    if (currentClusterer) {
+      currentClusterer.addMarker(marker.current);
+      return () => {
+        currentClusterer.removeMarker(marker.current);
+      };
+    }
+  }, [marker, clusterer, map]);
+  
+  // Create marker
+  useEffect(() => {
+    if (!marker.current) {
+      const newMarker = Object.assign(new google.maps.marker.AdvancedMarkerElement({
+        title: 'Echo ' + echo.id,
+        position: {lat: echo.coords.latitude, lng: echo.coords.longitude},
+        content: generateMarkerIcon(),
+        zIndex: 100
+      }), {echo});
+
+      marker.current = newMarker;
+    }
+
+    const markerClickListener = marker.current.addListener('click', (e) => {
       e.stop();
       onClick([echo]);
     });
 
-    clusterer.add(newMarker, newMarker.echo.id);
-
     return () => {
       markerClickListener.remove();
-      clusterer.remove(newMarker.echo.id);
     };
-  }, [echo, onClick, clusterer]);
+  }, [marker, echo, onClick]);
   
   return (<></>);
-}, (prevProps, nextProps) => prevProps.echo.id === nextProps.echo.id);
+};
 
 export default EchoMarker;
